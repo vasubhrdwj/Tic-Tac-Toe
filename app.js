60d0613f882b2e1a675397502a3c7acbc796f410
Vasu Bhardwaj <vasubhrdwj@gmail.com>
2025-02-13 18:45:10 +0530

    Added logic to switch players after every round

--- app.js
+++ app.js
@@ -15,7 +15,20 @@ function gameBoard() {
   }
 
   const markBoard = (row, col, player) => {
+    if (!isValid(row, col)) {
+      console.log("Select something valid dawg");
+      return -1;
+    }
+    if (board[row][col].getValue() !== 0) {
+      console.log("Already marked!!");
+      return -1;
+    }
     board[row][col].mark(player.token);
+  };
+
+  const isValid = (row, col) => {
+    if (row < 0 || row >= rows || col < 0 || col >= cols) return false;
+    else return true;
   };
 
   const printBoard = () => {
@@ -50,19 +63,16 @@ function gameController(
   let currentPlayer = players[0];
 
   const playRound = (row, col) => {
-    board.markBoard(row, col, currentPlayer);
+    const check = board.markBoard(row, col, currentPlayer);
+    if (check === -1) return;
     board.printBoard();
     switchPlayer();
   };
 
-  const switchPlayer = () => currentPlayer = (currentPlayer === players[0] ? players[1] : players[0]);
+  const switchPlayer = () =>
+    (currentPlayer = currentPlayer === players[0] ? players[1] : players[0]);
 
-
-  return {  playRound };
+  return { playRound };
 }
 
 let game = gameController();
-
-game.playRound(1, 1);
-game.playRound(1, 2);
-game.playRound(2, 2);
