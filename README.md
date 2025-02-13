6d9fb0600d5090e64d101c81bc6fb71be3d5b281
Vasu Bhardwaj <vasubhrdwj@gmail.com>
2025-02-13 17:19:59 +0530

    Added Readme, added gameBoard for board status

---
+++ README.md
@@ -0,0 +1,22 @@
+Steps: 
+1 - Create a 3 * 3 board = > rows = 3, cols = 3
+->  Blueprint:
+        Methods
+            functions
+    
+    
+    1 - gameBoard
+        a - Get board ( To get the current board states for UI)
+        b - playCell (To populate the current cell according to player number)
+        c - print board (print to the user console)
+
+    2 - function Cell()
+
+    3 - gameController
+        a - player object 
+        b - switchPlayer
+        3 - print round
+
+
+  
+
--- app.js
+++ app.js
@@ -1,3 +1,28 @@
-const bod = document.querySelector("body");
+function gameBoard() {
+  const rows = 3;
+  const cols = 3;
+  board = [];
 
-bod.innerHTML = "In my js file";
+  for (let i = 0; i < rows; i++) {
+    board[i] = [];
+    for (let j = 0; j < cols; j++) {
+      board[i].push(Cell());
+    }
+  }
+
+  printBoard = () => {
+    const boardValues = board.map((row) => row.map((cell) => cell.getValue()));
+    console.log(boardValues);
+  };
+
+  return {printBoard};
+}
+
+
+function Cell() {
+  let value = 0;
+  getValue = () => value;
+
+  return { getValue };
+}
+
